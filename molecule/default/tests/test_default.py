bdcc0e448bdd29550dd7b9c995466813f72798da
Josh Benner <josh@bennerweb.com>
2017-10-05 11:00:42 -0400

    Support daemon config

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -14,3 +14,13 @@ def test_docker_service(host):
     docker = host.service('docker')
     assert docker.is_enabled
     assert docker.is_running
+
+
+def test_docker_config(host):
+    file = host.file('/etc/docker/daemon.json')
+    assert file.exists
+    assert file.is_file
+    assert file.user == 'root'
+    assert file.group == 'root'
+    assert file.mode == 0o600
+    assert file.contains('syslog')
